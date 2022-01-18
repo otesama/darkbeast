c648bbf147274e43512d15186cee2709286b960d
otesama <c.jay@cloudflow.kr>
2022-01-19 01:19:01 +0900

    20200118-2

--- Upbit Auto Trade.py
+++ Upbit Auto Trade.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "ioeruRYNrXKHJMoPTijurQ2zfMTQEWl2mjx7I1Kj"
-secret = "YCUMfQOwOyKhN9qX3yQt0xWFgxxEMVaz8FoDTDYO"
+access = "input your key"     #본인 API ACCESS KEY"
+secret = "input your key"     #본인 API SECRET KEY
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
